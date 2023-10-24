ed62541ff5ec0597f5719ba5acece4876e9beed6
cxrxl <148091096+cxrxl@users.noreply.github.com>
2023-10-24 15:04:30 +0200

    Update vulnerable-flask-app.py

--- vulnerable-flask-app.py
+++ vulnerable-flask-app.py
@@ -4,7 +4,7 @@ from werkzeug.datastructures import Headers
 from werkzeug.utils import secure_filename
 import sqlite3
 
-
+#
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER']="/home/kali/Desktop/upload"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
